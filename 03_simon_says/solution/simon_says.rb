a067465f4ccd5aba4a353e78330a9732fffc9ca1
fender707 <fender707@rambler.ru>
2014-10-30 22:45:29 +0200

    %task03-3 - Added repeat function with default value

--- 03_simon_says/solution/simon_says.rb
+++ 03_simon_says/solution/simon_says.rb
@@ -1,8 +1,11 @@
-def echo(say)
-  say.downcase
-end
+  def echo(say)
+    say.downcase
+  end
 
-def shout(say)
-  say.upcase
-end
+  def shout(say)
+    say.upcase
+  end
 
+  def repeat(say='hello', count=0)
+    count == 0 ? ((say + " ") * 2).rstrip : ((say + " ") * count).rstrip
+  end
