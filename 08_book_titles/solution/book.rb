f9b085e24e2253f8863f017a44cc640a7553a6a4
fender707 <fender707@rambler.ru>
2014-10-31 23:03:59 +0200

    %task08-4 - Second word non work - must be Upcase

--- 08_book_titles/solution/book.rb
+++ 08_book_titles/solution/book.rb
@@ -5,12 +5,13 @@
 	def title=(title_new)
             little_words = %w{a an and the in of}
  	    words = title_new.split(" ")
-	    words = words[0].capitalize + words[1..words.length].each { |elem| 
+	    words = [words[0].capitalize] + words[1..words.length].each { |elem| 
             if little_words.include? elem 
-	    elem
+	      elem
 	    else 
 	      elem.capitalize
 	    end		
 		}
+	@title = words.join(" ")
 	end
   end
