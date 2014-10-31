2531022c670ea4442f1f8bdb36cef79d5e8d774f
fender707 <fender707@rambler.ru>
2014-10-31 23:13:01 +0200

    %task08-5 - Fixed branch else .capitalize! and done

--- 08_book_titles/solution/book.rb
+++ 08_book_titles/solution/book.rb
@@ -6,10 +6,10 @@
             little_words = %w{a an and the in of}
  	    words = title_new.split(" ")
 	    words = [words[0].capitalize] + words[1..words.length].each { |elem| 
-            if little_words.include? elem 
+            if little_words.include?(elem) 
 	      elem
 	    else 
-	      elem.capitalize
+	      elem.capitalize!
 	    end		
 		}
 	@title = words.join(" ")
