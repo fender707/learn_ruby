81e3a145aea30f5fd2f9994b39817ab76da2d53d
fender707 <fender707@rambler.ru>
2014-10-31 22:45:03 +0200

    %task08-3 - Added splitter >1 words

--- 08_book_titles/solution/book.rb
+++ 08_book_titles/solution/book.rb
@@ -1,9 +1,8 @@
 
   class Book
-     attr_accessor :title
+     attr_reader :title
 
 	def title=(title_new)
-	   words=title_new.split(" ")
-	   title_new[0].capitalize
+            words = title_new.split(" ")
 	end
   end
