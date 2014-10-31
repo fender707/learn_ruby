865db396562a1a3cf8525e6c3b09968f09313ae8
fender707 <fender707@rambler.ru>
2014-10-31 18:39:03 +0200

    %task05-1 - Added functions reverser, adder

--- 05_silly_blocks/solution/silly_blocks.rb
+++ 05_silly_blocks/solution/silly_blocks.rb
@@ -1,8 +1,20 @@
 def reverser
- if block_given? then b=yield
- c=b.to_s.split
- z=c.map { |i| i.reverse } 
- z.join(" ")
+  if block_given? then b=yield
+    c=b.to_s.split
+    z=c.map { |i| i.reverse } 
+    z.join(" ")
+  end
 end
+
+def adder(p)
+  if p==nil then
+       b=yield 
+       b+=1
+    else 
+       b=yield 
+       b+=p
+  end
 end
+
+
 
