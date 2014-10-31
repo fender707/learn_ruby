ed137d4fbd4567f2a1b9abe2020d9c7d8b152a49
fender707 <fender707@rambler.ru>
2014-10-31 15:33:25 +0200

    %task04-3 - Got words in array, need to join them

--- 04_pig_latin/solution/pig_latin.rb
+++ 04_pig_latin/solution/pig_latin.rb
@@ -1,16 +1,30 @@
   
   def translate(word)
+      q=word.index("qu") 
       len_mas_word=word.split(' ').length 
       mas_words=word.split(' ') 
       if len_mas_word==1 then
-        temp = word.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="u" || elem =="y" || elem =="i"} 
-        word[0]==temp[0] ? word << "ay" : word[word.index(temp[0])..word.length] + word[0..word.index(temp[0])-1] + "ay" 
+         if q!=nil then
+           temp = word.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="y" || elem =="i"} 
+         else
+           temp = word.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="u"  || elem =="o" || elem =="y" || elem =="i"} 
+	 end
+           word[0]==temp[0] ? word << "ay" : word[word.index(temp[0])..word.length] + word[0..word.index(temp[0])-1] + "ay" 
+         
       else 
-        iter=1
-        z=mas_words.map { |slovo| 
+          iter=1
+            if q!=nil then
+             z=mas_words.map { |slovo| 
+          temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o"|| elem =="y" || elem =="i"} 
+          slovo[0]==temp[0] ? slovo<< "ay" : slovo[iter]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
+            }
+            else
+
+          z=mas_words.map { |slovo| 
           temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="u" || elem =="y" || elem =="i"} 
           slovo[0]==temp[0] ? slovo<< "ay" : slovo[iter]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
 }
       z.join(" ")
+            end
       end
 end
