d07ffe16ef06cff60ade1a175e7c6ee39b2cc967
fender707 <fender707@rambler.ru>
2014-10-31 22:58:10 +0200

    %task08-4 - can't convert Array into String

--- 08_book_titles/solution/book.rb
+++ 08_book_titles/solution/book.rb
@@ -3,6 +3,14 @@
      attr_reader :title
 
 	def title=(title_new)
-            words = title_new.split(" ")
+            little_words = %w{a an and the in of}
+ 	    words = title_new.split(" ")
+	    words = words[0].capitalize + words[1..words.length].each { |elem| 
+            if little_words.include? elem 
+	    elem
+	    else 
+	      elem.capitalize
+	    end		
+		}
 	end
   end
