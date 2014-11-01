7ba66cce58303aade85ac54d46c97aa384f5918c
fender707 <fender707@rambler.ru>
2014-11-01 21:35:12 +0200

    %task12-7 - Added methods divide,times

--- 12_rpn_calculator/solution/rpn_calculator.rb
+++ 12_rpn_calculator/solution/rpn_calculator.rb
@@ -7,7 +7,7 @@
     end    
 
     def push(value)
-      @calculator << value
+      @calculator << value.to_f
     end
    
     def plus
@@ -18,6 +18,16 @@
     def minus
         if @calculator.length>=2 then minus=calculator[-2]-calculator[-1] else raise "calculator is empty" end
         @calculator[-2..-1]=minus
+    end
+
+    def divide
+        if @calculator.length>=2 then divide=calculator[-2]/calculator[-1] else raise "calculator is empty" end
+        @calculator[-2..-1]=divide
+    end
+
+    def times
+        if @calculator.length>=2 then times=calculator[-2]*calculator[-1] else raise "calculator is empty" end
+        @calculator[-2..-1]=times
     end
 
     def value
