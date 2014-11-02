0ea994c15bee2412b3c40b83a9255cd7200cb6f0
fender707 <fender707@rambler.ru>
2014-11-02 21:12:10 +0200

    %task15-5 - Added solution tens,teens,ones

--- 15_in_words/solution/in_words.rb
+++ 15_in_words/solution/in_words.rb
@@ -3,8 +3,9 @@
     def in_words
       ones = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
       tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-      teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'ten']  
-    if self < 10 then ones[self] end
-
+      teens = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']  
+    if self < 10 then ones[self] else
+    #if self > 9 && self < 20 then 
+     teens[self-10] end
     end
   end
