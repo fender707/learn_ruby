602beaf21315e87855f07459b40da62e57054471
fender707 <fender707@rambler.ru>
2014-11-02 13:20:11 +0200

    %task13-2 - Used method missing and put method name in result

--- 13_xml_document/solution/xml_document.rb
+++ 13_xml_document/solution/xml_document.rb
@@ -1,8 +1,13 @@
   
    class XmlDocument
-   
+     
+     def initialize param=false
+       @param=param
+     end   
+ 
      def method_missing(method_name)
-    
+       res=""
+       res<<"<#{method_name}/>"
      end
 
    end
