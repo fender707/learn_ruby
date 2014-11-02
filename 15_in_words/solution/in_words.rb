4bd16f09285ebd5ca702bc22b7e319df68850c1a
fender707 <fender707@rambler.ru>
2014-11-02 21:00:34 +0200

    %task15-3 - Added num=self

--- 15_in_words/solution/in_words.rb
+++ 15_in_words/solution/in_words.rb
@@ -4,5 +4,9 @@
       ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
       tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
       teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'ten']
+    num=self  
+    if num=0 then return "zero" end
+
+
     end
   end
