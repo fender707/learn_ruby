884c9f613222663696b6266126b7036dbeee3653
fender707 <fender707@rambler.ru>
2014-11-02 15:21:58 +0200

    %task13-7 - Need to fix some troubles with ' '

--- 13_xml_document/solution/xml_document.rb
+++ 13_xml_document/solution/xml_document.rb
@@ -3,18 +3,28 @@
      
      def initialize param=false
        @param=param
+       @levels=0
      end   
  
      def method_missing(method_name,*args,&block)
        if args.empty? then arguments={} else arguments=args[0] end
        res=""
-       res<<"<#{method_name}"
+       i=0
+       while i<=@levels && @levels!=0 && @param do 
+         res << " "  
+         i+=1   
+       end
+       res << "<#{method_name}"
        arguments.map { |key,value| res << " #{key}='#{value}'" }
          if block then  
-           res << ">" + yield 
-         res<<"</#{method_name}>"
+           res << ">"
+           res << "\n" if @param
+	   @levels += 1 
+           res <<  yield 
+           @levels -= 1 
+           res<<"</#{method_name}>"
          else
-       res << "/>"
+           res << "/>"
          end
      end
 
