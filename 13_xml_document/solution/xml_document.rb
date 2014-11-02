de35ca75133ef4e9a0f7a6785696c73d523427d5
fender707 <fender707@rambler.ru>
2014-11-02 14:47:30 +0200

    %task13-6 - Added &block in method_missing and used yield in body

--- 13_xml_document/solution/xml_document.rb
+++ 13_xml_document/solution/xml_document.rb
@@ -5,12 +5,17 @@
        @param=param
      end   
  
-     def method_missing(method_name,*args)
+     def method_missing(method_name,*args,&block)
        if args.empty? then arguments={} else arguments=args[0] end
        res=""
        res<<"<#{method_name}"
        arguments.map { |key,value| res << " #{key}='#{value}'" }
+         if block then  
+           res << ">" + yield 
+         res<<"</#{method_name}>"
+         else
        res << "/>"
+         end
      end
 
    end
