df2f579cfbd34bf1cab8987f8a8283c96f18e754
fender707 <fender707@rambler.ru>
2014-11-02 20:54:59 +0200

    %task15-2 - Wrote ones,tens,teens

---
+++ 15_in_words/solution/in_words.rb
@@ -0,0 +1,8 @@
+  
+  class Fixnum
+    def in_words
+      ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+      tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+      teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'ten']
+    end
+  end
