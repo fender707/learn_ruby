ed3af6935e4b4bca25b27cfedc2e236b947c5001
fender707 <fender707@rambler.ru>
2014-11-02 21:23:53 +0200

    %task15-6 - Added solution from 0 to 100

--- 15_in_words/solution/in_words.rb
+++ 15_in_words/solution/in_words.rb
@@ -4,8 +4,13 @@
       ones = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
       tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
       teens = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']  
-    if self < 10 then ones[self] else
-    #if self > 9 && self < 20 then 
-     teens[self-10] end
+      probel=" "
+    if self < 10 then ones[self] 
+     elsif self > 9 && self < 20 then teens[self-10] 
+     elsif self > 19 && self < 100 
+         if self % 10 == 0 then tens[self/10-1]
+                else tens[(self/10)-1] + probel + (self % 10).in_words 
+                end
     end
   end
+end
