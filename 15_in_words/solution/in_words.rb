d76977ecd4c42a995fb168d398ba924775980f0f
fender707 <fender707@rambler.ru>
2014-11-02 22:00:57 +0200

    %task15-8 - Need to fix if number is Bignum

--- 15_in_words/solution/in_words.rb
+++ 15_in_words/solution/in_words.rb
@@ -12,15 +12,20 @@
       teens = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']  
 
       probel=" "
-    if self < 10 then ones[self] 
+     if self < 10 then ones[self] 
      elsif self > 9 && self < 20 then teens[self-10] 
      elsif self > 19 && self < 100 
          if self % 10 == 0 then tens[self/10-1]
-                else tens[(self/10)-1] + probel + (self % 10).in_words 
-                end
-         else
+           else tens[(self/10)-1] + probel + (self % 10).in_words 
+         end
+     else
          max_num = HUNDR.keys.take_while { |i| i <= self}.last
-          first=(self/max_num).in_words + probel + HUNDR[max_num]
-    end
+         first=(self/max_num).in_words + probel + HUNDR[max_num]
+           if (self % max_num) != 0
+                first+ probel+ (self % max_num).in_words
+              else
+                 first
+              end    
+     end
   end
 end
