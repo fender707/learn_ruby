abf48ea46309ef6994ca7a74f5ee7352371f32d7
fender707 <fender707@rambler.ru>
2014-11-02 15:33:03 +0200

    %task13-8 - Need to add \n when used closed atributes

--- 13_xml_document/solution/xml_document.rb
+++ 13_xml_document/solution/xml_document.rb
@@ -9,11 +9,7 @@
      def method_missing(method_name,*args,&block)
        if args.empty? then arguments={} else arguments=args[0] end
        res=""
-       i=0
-       while i<=@levels && @levels!=0 && @param do 
-         res << " "  
-         i+=1   
-       end
+       res << ("  " * @levels) if @param
        res << "<#{method_name}"
        arguments.map { |key,value| res << " #{key}='#{value}'" }
          if block then  
@@ -22,10 +18,13 @@
 	   @levels += 1 
            res <<  yield 
            @levels -= 1 
+           res << ("  " * @levels) if @param
            res<<"</#{method_name}>"
          else
            res << "/>"
+	   res << "\n" if @param
          end
+	res
      end
 
    end
