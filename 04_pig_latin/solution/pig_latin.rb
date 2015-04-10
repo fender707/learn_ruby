b915447bc8b6e2a08e5634de360b6b27a1ecf4c1
fender707 <fender707@rambler.ru>
2015-04-10 23:08:34 +0300

    Refactoring pig_latin - delete trash code

--- 04_pig_latin/solution/pig_latin.rb
+++ 04_pig_latin/solution/pig_latin.rb
@@ -1,31 +1,30 @@
   
   def translate(word)
-      q=word.index("qu") 
-      len_mas_word=word.split(' ').length 
-      mas_words=word.split(' ') 
-      if len_mas_word==1 then
-         if q!=nil then
-           temp = word.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="y" || elem =="i"} 
-         else
-           temp = word.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="u"  || elem =="o" || elem =="y" || elem =="i"} 
-	 end
-           word[0]==temp[0] ? word << "ay" : word[word.index(temp[0])..word.length] + word[0..word.index(temp[0])-1] + "ay" 
-         
-      else 
-          iter=1
-            if q!=nil then
-             z=mas_words.map { |slovo| 
-             temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o"|| elem =="y" || elem =="i"} 
-             slovo[0]==temp[0] ? slovo<< "ay" : slovo[iter]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
-            }
-            else
-
-             z=mas_words.map { |slovo| 
-             temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="u" || elem =="y" || elem =="i"} 
-             slovo[0]==temp[0] ? slovo<< "ay" : slovo[iter]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
-}
-      
-            end
-z.join(" ")
+    temp = word.split('').find_all {|element| element == "a" || element == "e" || element == "o" || element == "y" || element == "i"}
+    temp_with_u = word.split('').find_all { |element| element == "a" || element == "e" || element == "o" || element == "u" || element == "y" || element == "i"}
+    unique_symbol=word.index("qu") 
+    if word.split(' ').length==1 then
+      if unique_symbol!=nil then
+        temp 
+      else
+        temp_with_u
       end
+        word[0]==temp[0] ? word << "ay" : word[word.index(temp[0])..word.length] + word[0..word.index(temp[0])-1] + "ay" 
+         
+    else 
+      iteration=1
+      if unique_symbol!=nil then
+        translates=word.split(' ').map { |slovo| 
+          temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o"|| elem =="y" || elem =="i"} 
+          slovo[0]==temp[0] ? slovo<< "ay" : slovo[iteration]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
+        }
+      else
+        translates=word.split(' ').map { |slovo| 
+        temp = slovo.split('').find_all{|elem| elem =="a" || elem =="e" || elem =="o" || elem =="u" || elem =="y" || elem =="i"} 
+        slovo[0]==temp[0] ? slovo<< "ay" : slovo[iteration]=slovo[slovo.index(temp[0])..slovo.length] + slovo[0..slovo.index(temp[0])-1] + "ay" 
+        }
+      
+      end
+  translates.join(" ")
+    end
 end
