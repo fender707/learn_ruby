8c436b0eef9e277a9a53c55c9a65e028e1efa42f
fender707 <fender707@rambler.ru>
2015-04-24 11:11:17 +0300

    Added tabulation

--- 04_pig_latin/solution/pig_latin.rb
+++ 04_pig_latin/solution/pig_latin.rb
@@ -1,15 +1,15 @@
 
   def translate(words)
     words.split(' ').map { |word|
-    place = word.index(/[aeouiy]/)
-    phoneme_length = word.scan(/(qu|sch)/).join.length
-    if phoneme_length > 0 
-      word_array = word.split(/(qu|sch)/)
-      word = word_array[2] + word_array[0] + word_array[1] + 'ay'
-    elsif place > 0   
-      word = word[place..word.length] + word[0...place] + 'ay'
-    else  
-      word << 'ay'
-    end
-  }.join(" ")
-end
+      place = word.index(/[aeouiy]/)
+      phoneme_length = word.scan(/(qu|sch)/).join.length
+      if phoneme_length > 0 
+        word_array = word.split(/(qu|sch)/)
+        word = word_array[2] + word_array[0] + word_array[1] + 'ay'
+      elsif place > 0   
+        word = word[place..word.length] + word[0...place] + 'ay'
+      else  
+        word << 'ay'
+      end
+    }.join(" ")
+  end
