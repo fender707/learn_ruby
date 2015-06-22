ae17158847ae2507e76165e7b186167906669abb
fender707 <fender707@rambler.ru>
2015-06-22 23:02:45 +0300

    Added tabulation

--- 09_timer/solution/timer.rb
+++ 09_timer/solution/timer.rb
@@ -2,14 +2,13 @@
   class Timer
     attr_accessor :seconds
      def initialize
-       @seconds=0
+       @seconds = 0
      end
 
      def time_string
-       hours=@seconds/3600
-       minutes=@seconds%3600/60
-       sec=@seconds%3600%60
-
+       hours   = @seconds/3600
+       minutes = @seconds%3600/60
+       sec     = @seconds%3600%60
        sprintf("%02d:%02d:%02d", hours, minutes, sec)
      end
   end
