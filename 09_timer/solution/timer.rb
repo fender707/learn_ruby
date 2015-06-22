b3b55a27075d550393f9ab566b0a889dd407ff9f
fender707 <fender707@rambler.ru>
2015-06-22 23:03:36 +0300

    Added spaces

--- 09_timer/solution/timer.rb
+++ 09_timer/solution/timer.rb
@@ -6,9 +6,9 @@
      end
 
      def time_string
-       hours   = @seconds/3600
-       minutes = @seconds%3600/60
-       sec     = @seconds%3600%60
+       hours   = @seconds / 3600
+       minutes = @seconds % 3600 / 60
+       sec     = @seconds % 3600 % 60
        sprintf("%02d:%02d:%02d", hours, minutes, sec)
      end
   end
