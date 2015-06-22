0ca610a60d3fa4d445184e9d4466786e42ed2778
fender707 <fender707@rambler.ru>
2015-06-22 22:56:51 +0300

    Changed tabulation

--- 08_book_titles/solution/book.rb
+++ 08_book_titles/solution/book.rb
@@ -3,15 +3,14 @@
      attr_reader :title
 
 	def title=(title_new)
-            little_words = %w{a an and the in of}
- 	    words = title_new.split(" ")
-	    words = [words[0].capitalize] + words[1..words.length].each { |elem| 
-            if little_words.include?(elem) 
-	      elem
-	    else 
-	      elem.capitalize!
-	    end		
-		}
-	@title = words.join(" ")
+          little_words = %w{a an and the in of}
+          words = title_new.split(" ")
+          words = [words[0].capitalize] + words[1..words.length].each { |elem| 
+          if little_words.include?(elem) 
+	    elem
+	  else 
+	    elem.capitalize!
+	  end			}
+ 	  @title = words.join(" ")
 	end
   end
