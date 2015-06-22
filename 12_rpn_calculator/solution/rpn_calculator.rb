7e1e0d082266fa00dc219a06e2fe6e22fc514adc
fender707 <fender707@rambler.ru>
2015-06-22 23:29:31 +0300

    Added spaces

--- 12_rpn_calculator/solution/rpn_calculator.rb
+++ 12_rpn_calculator/solution/rpn_calculator.rb
@@ -3,7 +3,7 @@
     attr_accessor :calculator
 
     def initialize
-      @calculator=[]
+      @calculator = []
     end    
 
     def push(value)
@@ -11,39 +11,39 @@
     end
    
     def plus
-      if @calculator.length>=2 then 
-        suma=calculator[-2]+calculator[-1] 
+      if @calculator.length >= 2 then 
+        suma = calculator[-2] + calculator[-1] 
       else 
         raise "calculator is empty" 
       end
-      @calculator[-2..-1]=suma
+      @calculator[-2..-1] = suma
     end
 
     def minus
-      if @calculator.length>=2 then 
-        minus=calculator[-2]-calculator[-1] 
+      if @calculator.length >= 2 then 
+        minus = calculator[-2] - calculator[-1] 
       else 
         raise "calculator is empty" 
       end
-      @calculator[-2..-1]=minus
+      @calculator[-2..-1] = minus
     end
 
     def divide
-        if @calculator.length>=2 then 
-          divide=calculator[-2]/calculator[-1] 
+        if @calculator.length >= 2 then 
+          divide = calculator[-2]/calculator[-1] 
         else 
           raise "calculator is empty" 
         end
-        @calculator[-2..-1]=divide
+        @calculator[-2..-1] = divide
     end
 
     def times
-      if @calculator.length>=2 then 
-        times=calculator[-2]*calculator[-1] 
+      if @calculator.length >= 2 then 
+        times=calculator[-2] * calculator[-1] 
       else 
         raise "calculator is empty" 
       end
-      @calculator[-2..-1]=times
+      @calculator[-2..-1] = times
     end
 
     def value
